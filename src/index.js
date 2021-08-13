baa6cea76a2a0458078c9f135f8f911f97e3115f
alya783 <76521831+alya783@users.noreply.github.com>
2021-08-13 14:21:50 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 exports.min = function min (array) {
-    if (array.lenght === 0 || array === undefined){
-        return 0;
+   if(arguments.length === 0 || array.length === 0){
+	  return 0;
     }
     let min = array[0];
     for(let i = 1; i < array.length; i++){
@@ -13,9 +13,9 @@ exports.min = function min (array) {
 }
 
 exports.max = function max (array) {
-  if (array.lenght === 0 || array === undefined){
-        return 0;
-    }
+  if(arguments.length === 0 || array.length === 0){
+	  return 0;
+  }
   let max = array[0];
     for(let i = 1; i < array.length; i++){
         if(array[i] > max){
@@ -27,9 +27,9 @@ exports.max = function max (array) {
 }
 
 exports.avg = function avg (array) {
-  if (array.lenght === 0 || array === undefined){
-        return 0;
-    }
+  if(arguments.length === 0 || array.length === 0){
+	  return 0;
+  }
   let sum = 0;
     for(let i = 0; i < array.length; i++){
         sum += array[i];
